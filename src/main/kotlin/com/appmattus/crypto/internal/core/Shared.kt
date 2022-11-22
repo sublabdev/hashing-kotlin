90de9e70cec4d9c63bdd8615cb35079c5471e8b1
Alex Oakley <alex@alexoakley.com>
2022-11-22 02:53:03 -0800

    Provided Keccak F1600 implementation by modifying files from underlying libraries and testing with matches from Rust result

---
+++ src/main/kotlin/com/appmattus/crypto/internal/core/Shared.kt
@@ -0,0 +1,37 @@
+package com.appmattus.crypto.internal.core
+
+/**
+ * Encode a 64-bit integer with little-endian convention.
+ *
+ * @param [value]   the integer to encode
+ * @param dst   the destination buffer
+ * @param off   the destination offset
+ */
+internal fun encodeLELong(value: Long, dst: ByteArray, off: Int) {
+    dst[off + 0] = value.toByte()
+    dst[off + 1] = (value.toInt() ushr 8).toByte()
+    dst[off + 2] = (value.toInt() ushr 16).toByte()
+    dst[off + 3] = (value.toInt() ushr 24).toByte()
+    dst[off + 4] = (value ushr 32).toByte()
+    dst[off + 5] = (value ushr 40).toByte()
+    dst[off + 6] = (value ushr 48).toByte()
+    dst[off + 7] = (value ushr 56).toByte()
+}
+
+/**
+ * Decode a 64-bit little-endian integer.
+ *
+ * @param buf   the source buffer
+ * @param off   the source offset
+ * @return the decoded integer
+ */
+internal fun decodeLELong(buf: ByteArray, off: Int): Long {
+    return (buf[off + 0].toLong() and 0xFF
+            or ((buf[off + 1].toLong() and 0xFF) shl 8)
+            or ((buf[off + 2].toLong() and 0xFF) shl 16)
+            or ((buf[off + 3].toLong() and 0xFF) shl 24)
+            or ((buf[off + 4].toLong() and 0xFF) shl 32)
+            or ((buf[off + 5].toLong() and 0xFF) shl 40)
+            or ((buf[off + 6].toLong() and 0xFF) shl 48)
+            or ((buf[off + 7].toLong() and 0xFF) shl 56))
+}
--- src/main/kotlin/dev/sublab/hashing/hashers/Keccak.kt
+++ src/main/kotlin/dev/sublab/hashing/hashers/Keccak.kt
@@ -1,19 +1,21 @@
 package dev.sublab.hashing.hashers
 
 import com.appmattus.crypto.Algorithm
+import com.appmattus.crypto.internal.core.sphlib.Keccak1600
+import com.appmattus.crypto.internal.core.sphlib.prepareForKeccakF1600
 import dev.sublab.hashing.Hashing
 import dev.sublab.hashing.InvalidHashOutputSizeException
 
 private fun ByteArray.keccak(outputSize: Int) =
     when (outputSize) {
-        224 -> Algorithm.Keccak224
-        256 -> Algorithm.Keccak256
-        288 -> Algorithm.Keccak288
-        384 -> Algorithm.Keccak384
-        512 -> Algorithm.Keccak512
+        224 -> Algorithm.Keccak224.createDigest()
+        256 -> Algorithm.Keccak256.createDigest()
+        288 -> Algorithm.Keccak288.createDigest()
+        384 -> Algorithm.Keccak384.createDigest()
+        512 -> Algorithm.Keccak512.createDigest()
+        1600 -> Keccak1600()
         else -> throw InvalidHashOutputSizeException(outputSize)
     }
-    .createDigest()
     .apply { update(this@keccak) }
     .digest()
 
@@ -23,3 +25,4 @@ fun Hashing.keccak256() = keccak(256)
 fun Hashing.keccak288() = keccak(288)
 fun Hashing.keccak384() = keccak(384)
 fun Hashing.keccak512() = keccak(512)
+fun Hashing.keccak1600() = value.toByteArray().prepareForKeccakF1600().keccak(1600)
